881b6948c2c58494f0bcd88e05d3ac2f5aabe3c2
André Ribeiro <ribeirolimand@gmail.com>
2021-10-14 22:26:23 -0300

    webhook

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI, Form, Response
 
 app = FastAPI()
  
-@app.get("/helloworld/")
-def root ():
+@app.get("/helloworld")
+def root():
   return {"message": "Hello World!"}
 
 
 @app.post("/hook")
-async def chat(From: str = Form(...), Body: str = Form(...)):
+async def chat():
 #   response = MessagingResponse()
-   msg = {f"Hi {From}, you said: {Body}"}
-   return Response(content=str(response), media_type="application/xml")
+   msg = {f"Hi, you said: ok"}
+   return msg
