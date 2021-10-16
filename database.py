b1fa0a4f0745cef2f8e0fa3d6e444c9c7875ddcb
André Ribeiro <ribeirolimand@gmail.com>
2021-10-16 12:29:59 -0300

    postgres

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
+DATABASE_URL = os.environ['DATABASE_URL']
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:vxjrxgdmrsrtrm:23f4c32e71081e7fe9213dbd8c9592a8aa8371a73db14c4e087de19374c89f0c@ec2-3-231-40-72.compute-1.amazonaws.com:5432/dbv392m50ekkf4"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:vxjrxgdmrsrtrm:23f4c32e71081e7fe9213dbd8c9592a8aa8371a73db14c4e087de19374c89f0c@ec2-3-231-40-72.compute-1.amazonaws.com:5432/dbv392m50ekkf4"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
