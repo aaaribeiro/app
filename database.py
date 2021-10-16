d12c767a986d94d29397eb198f372bd12ef471d8
André Ribeiro <ribeirolimand@gmail.com>
2021-10-16 12:34:47 -0300

    postgres

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 import os
 DATABASE_URL = os.environ['DATABASE_URL']
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 #SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:vxjrxgdmrsrtrm:23f4c32e71081e7fe9213dbd8c9592a8aa8371a73db14c4e087de19374c89f0c@ec2-3-231-40-72.compute-1.amazonaws.com:5432/dbv392m50ekkf4"
 engine = create_engine(DATABASE_URL)
